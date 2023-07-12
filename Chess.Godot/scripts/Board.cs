16b868a8fae62da935affbbb63954c5c4d2c71c4
Askold <askoldmakaruk@gmail.com>
2023-07-12 14:31:11 +0300

    oush

--- Chess.Godot/scripts/Board.cs
+++ Chess.Godot/scripts/Board.cs
@@ -1,5 +1,10 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
+public record Cell(int X, int Y);
+
+public record CellRect(Cell Cell, ColorRect Rect);
 
 public partial class Board : Godot.GridContainer
 {
@@ -16,10 +21,64 @@ public partial class Board : Godot.GridContainer
             };
             AddChild(colorRect);
         }
+        //redraw
+        QueueRedraw();
+    }
+
+    private List<CellRect> _cells = new List<CellRect>();
+
+    private Cell _selectedCell = new Cell(0, 0);
+
+    public override void _Draw()
+    {
+        var cellRect = GetCellRect(_selectedCell);
+        DrawRect(cellRect.Rect.GetRect(), Colors.Red);
+    }
+
+    private CellRect GetCellRect(Cell cell)
+    {
+        var index = cell.X + cell.Y * 8;
+        return new CellRect(cell, (ColorRect)GetChild(index));
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        // if input key is h move left
+        if (@event.IsActionPressed("ui_left"))
+        {
+            if (_selectedCell.X > 0)
+            {
+                _selectedCell = new Cell(_selectedCell.X - 1, _selectedCell.Y);
+            }
+        }
+        // if input key is j move down
+        else if (@event.IsActionPressed("ui_down"))
+        {
+            if (_selectedCell.Y < 7)
+            {
+                _selectedCell = new Cell(_selectedCell.X, _selectedCell.Y + 1);
+            }
+        }
+        // if input key is k move up
+        else if (@event.IsActionPressed("ui_up"))
+        {
+            if (_selectedCell.Y > 0)
+            {
+                _selectedCell = new Cell(_selectedCell.X, _selectedCell.Y - 1);
+            }
+        }
+        // if input key is l move right
+        else if (@event.IsActionPressed("ui_right"))
+        {
+            if (_selectedCell.X < 7)
+            {
+                _selectedCell = new Cell(_selectedCell.X + 1, _selectedCell.Y);
+            }
+        }
     }
 }
